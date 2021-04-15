b31df715fc1d2273596f62e30006cd9f82cda524
Ivan Shashkov <mr.xaski.2002@gmail.com>
2021-04-15 14:35:14 +0700

    add scroll

---
+++ js/script.js
@@ -0,0 +1,17 @@
+(function () {
+	function scrollHorizontally(e) {
+		e = window.event || e;
+		var delta = Math.max(-1, Math.min(1, (e.wheelDelta || -e.detail)));
+		document.querySelector(".models-kit").scrollLeft -= (delta * 40); // Multiplied by 40
+		e.preventDefault();
+	}
+	if (document.querySelector(".models-kit").addEventListener) {
+		// IE9, Chrome, Safari, Opera
+		document.querySelector(".models-kit").addEventListener("mousewheel", scrollHorizontally, false);
+		// Firefox
+		document.querySelector(".models-kit").addEventListener("DOMMouseScroll", scrollHorizontally, false);
+	} else {
+		// IE 6/7/8
+		document.querySelector(".models-kit").attachEvent("onmousewheel", scrollHorizontally);
+	}
+})();
