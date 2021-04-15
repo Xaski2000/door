74f9d193d4972fbccf0e5a6cba5c44e2ff54eba0
Ivan Shashkov <mr.xaski.2002@gmail.com>
2021-04-15 19:59:03 +0700

    add slider

--- js/script.js
+++ js/script.js
@@ -1,17 +1,30 @@
-(function () {
-	function scrollHorizontally(e) {
-		e = window.event || e;
-		var delta = Math.max(-1, Math.min(1, (e.wheelDelta || -e.detail)));
-		document.querySelector(".models-kit").scrollLeft -= (delta * 40); // Multiplied by 40
-		e.preventDefault();
-	}
-	if (document.querySelector(".models-kit").addEventListener) {
-		// IE9, Chrome, Safari, Opera
-		document.querySelector(".models-kit").addEventListener("mousewheel", scrollHorizontally, false);
-		// Firefox
-		document.querySelector(".models-kit").addEventListener("DOMMouseScroll", scrollHorizontally, false);
-	} else {
-		// IE 6/7/8
-		document.querySelector(".models-kit").attachEvent("onmousewheel", scrollHorizontally);
-	}
-})();
+document.addEventListener('DOMContentLoaded', function () {
+	// получаем корневой элемент слайдера
+	const $slider = document.querySelector('[data-slider="chiefslider"]');
+	// выполняем инициализацию
+	new ChiefSlider($slider, {
+		loop: true
+	});
+});
+
+// document.querySelector('#first').on('click', function () {
+// 	alert("done");
+// });
+
+// (function () {
+// 	function scrollHorizontally(e) {
+// 		e = window.event || e;
+// 		var delta = Math.max(-1, Math.min(1, (e.wheelDelta || -e.detail)));
+// 		document.querySelector(".models-kit").scrollLeft -= (delta * 40); // Multiplied by 40
+// 		e.preventDefault();
+// 	}
+// 	if (document.querySelector(".models-kit").addEventListener) {
+// 		// IE9, Chrome, Safari, Opera
+// 		document.querySelector(".models-kit").addEventListener("mousewheel", scrollHorizontally, false);
+// 		// Firefox
+// 		document.querySelector(".models-kit").addEventListener("DOMMouseScroll", scrollHorizontally, false);
+// 	} else {
+// 		// IE 6/7/8
+// 		document.querySelector(".models-kit").attachEvent("onmousewheel", scrollHorizontally);
+// 	}
+// })();
