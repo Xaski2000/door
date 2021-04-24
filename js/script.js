b81e2f093aeafb0c780b18a9aad53b3abdb8533c
Ivan Shashkov <mr.xaski.2002@gmail.com>
2021-04-24 15:26:14 +0700

    upd 24.04

--- js/script.js
+++ js/script.js
@@ -1,146 +1,181 @@
-document.addEventListener('DOMContentLoaded', function () {
-	// получаем корневой элемент слайдера
-	// const $slider = document.querySelector('[data-slider="chiefslider"]');
-	// // выполняем инициализацию
-	// new ChiefSlider($slider, {
-	// 	loop: true
-	// });
+// const testbtn = document.querySelector('.test-btn'),
+// 		testbtn2 = document.querySelector('.test-btn-2'),
+// 		testbtn3 = document.querySelector('.test-btn-3'),
+// 		test = document.querySelector('.test-1'),
+// 		tests = document.querySelectorAll('.tesst'),
+// 		test1 = document.querySelector('.test-1-1'),
+// 		test2 = document.querySelector('.test-1-2'),
+// 		test3 = document.querySelector('.test-1-3');
 
-	const $sliders = document.querySelectorAll('[data-slider="chiefslider"]');
-	$sliders.forEach(function ($slider) {
-		new ChiefSlider($slider, {
-			loop: true
-		});
+// testbtn3.addEventListener('click', () => {
+// 	if (window.getComputedStyle(test1).display == 'none') {
+// 		tests.forEach(item => {
+// 			item.classList.remove('test-none');
+// 			item.classList.add('test-block');
+// 		});
+// 	} else {
+// 		tests.forEach(item => {
+// 			item.classList.remove('test-block');
+// 			item.classList.add('test-none');
+// 		});
+// 	}
+// });
+
+
+const modResBox = document.querySelectorAll('.models-result-box'),
+		facResBox = document.querySelectorAll('.facing-result-box'),
+		knobResBox = document.querySelectorAll('.knob-result-box');
+
+const modOutItem = document.querySelectorAll('.modelsOut__item'),
+		modOutItem1 = document.querySelector('.modelsOut__item-1'),
+		modOutItem2 = document.querySelector('.modelsOut__item-2'),
+		modOutItem3 = document.querySelector('.modelsOut__item-3');
+
+const facOutItem = document.querySelectorAll('.facingOut__item'),
+		facOutItem1 = document.querySelector('.facingOut__item-1'),
+		facOutItem2 = document.querySelector('.facingOut__item-2');
+
+const facInItem = document.querySelectorAll('.facingIn__item'),
+		facInItem1 = document.querySelector('.facingIn__item-1'),
+		facInItem2 = document.querySelector('.facingIn__item-2'),
+		facInItem3 = document.querySelector('.facingIn__item-3');
+
+const knobItem1 = document.querySelector('.knob__item-1'),
+		knobItem2 = document.querySelector('.knob__item-2');
+
+//
+
+const modOutRes = document.querySelectorAll('.modelsOut-result'),
+		facOutRes = document.querySelectorAll('.facingOut-result'),
+		knobOutRes = document.querySelectorAll('.knobOut-result');
+
+const modInRes = document.querySelectorAll('.modelsIn-result'),
+		facInRes = document.querySelectorAll('.facingIn-result'),
+		knobInRes = document.querySelectorAll('.knobIn-result');
+
+// Корпуса
+
+const modRes1 = document.querySelector('.models-result-1'),
+		modRes2 = document.querySelector('.models-result-2'),
+		modRes3 = document.querySelector('.models-result-3'),
+		modRes4 = document.querySelector('.models-result-4');
+
+// Внешние панели
+
+const facOutRes1 = document.querySelector('.facingOut-result-1'),
+		facOutRes2 = document.querySelector('.facingOut-result-2');
+
+// Внутренние панели
+
+const facInRes1 = document.querySelector('.facingIn-result-1'),
+		facInRes2 = document.querySelector('.facingIn-result-2'),
+		facInRes3 = document.querySelector('.facingIn-result-3');
+
+// const facRes1 = document.querySelector('.facing-result-1'),
+// 		facRes2 = document.querySelector('.facing-result-2');
+
+// Внешние ручки
+
+const knobRes1 = document.querySelector('.knob-result-1');
+
+function modOutResNullify() {
+	modOutRes.forEach(item => {
+		item.classList.remove('block');
 	});
-
-});
-
-const series1 = document.querySelector('.series-1'),
-	series2 = document.querySelector('.series-2'),
-	series3 = document.querySelector('.series-3'),
-	series4 = document.querySelector('.series-4'),
-	series5 = document.querySelector('.series-5'),
-	facingItem = document.querySelectorAll('.facing__item'),
-	facingItem1 = document.querySelectorAll('.facing__item-series-1'),
-	facingItem2 = document.querySelectorAll('.facing__item-series-2'),
-	facingItem3 = document.querySelectorAll('.facing__item-series-3'),
-	facingItem4 = document.querySelectorAll('.facing__item-series-4'),
-	facingItem5 = document.querySelectorAll('.facing__item-series-5');
-
-	function hideFacingItem () {
-		facingItem.forEach(item => {
-			item.classList.remove('test-block');
-			item.classList.add('test-none');
-		});
-	}
-
-series1.addEventListener('click', () => {
-	// facingItem.forEach(item => {
-	// 	item.classList.remove('test-block');
-	// 	item.classList.add('test-none');
-	// });
-
-	hideFacingItem();
-
-	facingItem1.forEach(item => {
-		item.classList.add('test-block');
+	modOutItem.forEach(item => {
+		item.classList.remove('border');
 	});
-});
-series2.addEventListener('click', () => {
-	facingItem.forEach(item => {
-		item.classList.remove('test-block');
-		item.classList.add('test-none');
+}
+
+function modInResNullify() {
+	modInRes.forEach(item => {
+		item.classList.remove('block');
 	});
-
-	facingItem2.forEach(item => {
-		item.classList.add('test-block');
+	modInItem.forEach(item => {
+		item.classList.remove('border');
 	});
-});
-series3.addEventListener('click', () => {
-	facingItem.forEach(item => {
-		item.classList.remove('test-block');
-		item.classList.add('test-none');
+}
+
+function facOutResNullify() {
+	facOutRes.forEach(item => {
+		item.classList.remove('block');
 	});
+}
 
-	facingItem3.forEach(item => {
-		item.classList.add('test-block');
+function facInResNullify() {
+	facInRes.forEach(item => {
+		item.classList.remove('block');
 	});
-});
-series4.addEventListener('click', () => {
-	facingItem.forEach(item => {
-		item.classList.remove('test-block');
-		item.classList.add('test-none');
+}
+
+function knobOutResNullify() {
+	knobOutRes.forEach(item => {
+		item.classList.remove('block');
 	});
+}
 
-	facingItem4.forEach(item => {
-		item.classList.add('test-block');
+function knobInResNullify() {
+	knobInRes.forEach(item => {
+		item.classList.remove('block');
 	});
-});
-series5.addEventListener('click', () => {
-	facingItem.forEach(item => {
-		item.classList.remove('test-block');
-		item.classList.add('test-none');
-	});
+}
 
-	facingItem5.forEach(item => {
-		item.classList.add('test-block');
-	});
-});
+// Внешние корпуса
 
-const testbtn = document.querySelector('.test-btn'),
-		testbtn2 = document.querySelector('.test-btn-2'),
-		testbtn3 = document.querySelector('.test-btn-3'),
-		test = document.querySelector('.test-1'),
-		tests = document.querySelectorAll('.tesst'),
-		test1 = document.querySelector('.test-1-1'),
-		test2 = document.querySelector('.test-1-2'),
-		test3 = document.querySelector('.test-1-3');
-
-testbtn.addEventListener('click', () => {
-	if (window.getComputedStyle(test).display == 'none') {
-		test.classList.remove("test-none");
-		test.classList.add("test-block");
-	} else {
-		test.classList.remove("test-block");
-		test.classList.add("test-none");
-	}
-	const a = window.getComputedStyle(test).display;
-	console.log('большой '+a);
+modOutItem1.addEventListener('click', () => {
+	modOutResNullify();
+	// modRes2.classList.remove('block');
+	modRes1.classList.add('block');
+	modRes4.classList.add('block');
+	modOutItem1.classList.add('border');
 });
 
-testbtn2.addEventListener('click', () => {
-	if (window.getComputedStyle(test1).display == 'none') {
-		test1.classList.remove("test-none");
-		test1.classList.add("test-block");
-	} else {
-		test1.classList.remove("test-block");
-		test1.classList.add("test-none");
-	}
-	const a = window.getComputedStyle(test1).display;
-	console.log('маленький '+a);
-});
-testbtn3.addEventListener('click', () => {
-	if (window.getComputedStyle(test1).display == 'none') {
-		tests.forEach(item => {
-			item.classList.remove('test-none');
-			item.classList.add('test-block');
-		});
-	} else {
-		tests.forEach(item => {
-			item.classList.remove('test-block');
-			item.classList.add('test-none');
-		});
-	}
-
+modOutItem2.addEventListener('click', () => {
+	modOutResNullify();
+	modRes2.classList.add('block');
+	modRes4.classList.add('block');
+	modOutItem2.classList.add('border');
 });
 
+modOutItem3.addEventListener('click', () => {
+	modOutResNullify();
+	modRes3.classList.add('block');
+	modRes4.classList.add('block');
+	modOutItem3.classList.add('border');
+});
 
-test1.addEventListener('click', () => {
-	console.log('Клик 1-1!');
+// Внешние панели
+
+facOutItem1.addEventListener('click', () => {
+	facOutResNullify();
+	facOutRes1.classList.add('block');
 });
-test2.addEventListener('click', () => {
-	console.log('Клик 1-2!');
+
+facOutItem2.addEventListener('click', () => {
+	facOutResNullify();
+	facOutRes2.classList.add('block');
 });
-test3.addEventListener('click', () => {
-	console.log('Клик 1-3!');
+
+// Внутренние панели
+
+facInItem1.addEventListener('click', () => {
+	facInResNullify();
+	facInRes1.classList.add('block');
+});
+
+facInItem2.addEventListener('click', () => {
+	facInResNullify();
+	facInRes2.classList.add('block');
+});
+
+facInItem3.addEventListener('click', () => {
+	facInResNullify();
+	facInRes3.classList.add('block');
+});
+
+// Внешние ручки
+
+knobItem1.addEventListener('click', () => {
+	knobOutResNullify();
+	knobRes1.classList.add('block');
 });
